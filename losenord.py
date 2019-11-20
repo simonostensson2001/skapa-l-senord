d574e26f5a96555b40b009fdcd9728997f8f9a10
simonostensson2001 <simonostensson2001@users.noreply.github.com>
2019-11-20 13:27:01 +0100

    färdigt

--- losenord.py
+++ losenord.py
@@ -1,6 +1,6 @@
 
-import random
-import array
+#import random
+#import array
 #s = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 #s ={"small" : "abcdefghijklmnopqrstuvwxyz",
 #    "big" : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -24,21 +24,50 @@ import array
 
 
 
-MAX_LEN = 12
+#MAX_LEN = 12
 
-LOCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
-UPCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-DIGITS = "0123456789"
-SYMBOLS = "!@#$%^&*()?"
-COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
-rand_digit = random.choice(DIGITS) 
-rand_upper = random.choice(UPCASE_CHARACTERS) 
-rand_lower = random.choice(LOCASE_CHARACTERS) 
-rand_symbol = random.choice(SYMBOLS)
-temp_pass = temp_pass + random.choice(COMBINED_LIST) 
+#LOCASE_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+#UPCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#DIGITS = "0123456789"
+#SYMBOLS = "!@#$%^&*()?"
+#COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+#rand_digit = random.choice(DIGITS) 
+#rand_upper = random.choice(UPCASE_CHARACTERS) 
+#rand_lower = random.choice(LOCASE_CHARACTERS) 
+#rand_symbol = random.choice(SYMBOLS)
+#temp_pass = temp_pass + random.choice(COMBINED_LIST) 
 
 
 #p = "".join(random.sample(losen ))
+#f= open("losenordfil.txt","a") 
+#f.write(p)
+#f.write("\n")
+
+chars = "abcdefghijklmnopqrstuvwxyz"
+chars1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+chars2 = "1234567890"
+chars3 = "!%&/()=#"
+import random 
+import string
+password = ""
+password = ""
+for c in range (3):
+    password += random.choice(chars)
+    password += random.choice(chars1)
+    password += random.choice(chars2)
+    password += random.choice(chars3)
+
+password = ''.join(random.sample(password,len(password)))
 f= open("losenordfil.txt","a") 
-f.write(p)
+f.write(password)
 f.write("\n")
+
+#print(password)
+#print(len(password)) 
+
+
+
+
+
+
+
